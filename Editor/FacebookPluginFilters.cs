c7ac5769da0de24b1a190dc5611ccd2dc4b3b8f7
LyneXiao <lixonxs@icloud.com>
2022-10-19 09:17:54 +0800

    调整SDK

--- Editor/FacebookPluginFilters.cs
+++ Editor/FacebookPluginFilters.cs
@@ -24,6 +24,7 @@ namespace PluginSet.Facebook.Editor
             PluginFilter.RegisterFilter("com.pluginset.facebook/Plugins/Android", FilterPlugins);
             PluginFilter.RegisterFilter("com.pluginset.facebook/Plugins/Android/libs", FilterPlugins);
             PluginFilter.RegisterFilter("com.pluginset.facebook/Plugins/iOS", FilterPlugins);
+            PluginFilter.RegisterFilter("com.pluginset.facebook/Plugins/iOS/Swift", FilterPlugins);
             PluginFilter.RegisterFilter("com.pluginset.facebook/Plugins/Canvas", FilterPlugins);
             PluginFilter.RegisterFilter("com.pluginset.facebook/Plugins/Settings", FilterPlugins);
             PluginFilter.RegisterFilter("com.pluginset.facebook/Plugins/Windows", FilterPlugins);
